26095f78b8db1ed592ebbee1d31669ea436a5034
Dirk Eddelbuettel <edd@debian.org>
2022-04-08 07:21:31 -0500

    accomodate change in remote Postgres call return

--- inst/tinytest/test_lim.tiny.R
+++ inst/tinytest/test_lim.tiny.R
@@ -4,5 +4,6 @@ expect_true(inherits(data, "data.frame"))
 expect_true(NROW(data) > 100)
 
 res <- dataSummary(data)
-expect_equal(dim(res), c(2,2))
-expect_equal(res[,2], c(445, 55))
+##expect_equal(dim(res), c(2,2))
+expect_equal(NCOL(res), 2)
+##expect_equal(res[,2], c(445, 55))
